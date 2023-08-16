b66be57a97e10901c9f354b7df1cb44243e15f44
samivilana <melanisamantha1803@hotmail.com>
2023-08-15 21:56:22 -0500

    Prueba 2

--- MiChaucherita/src/main/java/HolaMundo.java
+++ MiChaucherita/src/main/java/HolaMundo.java
@@ -1,10 +1,9 @@
 
-public class Prueba {
+public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Hola mundo");
+		System.out.println("Hola");
 
 	}
 
