a9471512f3daa1c66f1c81b6438a3b806d2e4dd7
samivilana <melanisamantha1803@hotmail.com>
2023-08-15 22:43:45 -0500

    Prueba get

--- MiChaucherita/src/main/java/controlador/Acceder.java
+++ MiChaucherita/src/main/java/controlador/Acceder.java
@@ -28,6 +28,7 @@ public class Acceder extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Ingreso por GET");
 	}
 
 	/**
