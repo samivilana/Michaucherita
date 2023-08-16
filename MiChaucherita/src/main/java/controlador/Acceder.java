dc223a9298e28c81305999dd3dd410c8fdce384d
samivilana <melanisamantha1803@hotmail.com>
2023-08-15 22:46:17 -0500

    Prueba

--- MiChaucherita/src/main/java/controlador/Acceder.java
+++ MiChaucherita/src/main/java/controlador/Acceder.java
@@ -29,7 +29,6 @@ public class Acceder extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		System.out.println("Ingreso por GET");
-		
 	}
 
 	/**
